b3cb035d536776a4eb382e7cef9c1bf0a1683fe5
Sandip Nepal <sandip.nepal.69@gmail.com>
2022-05-26 14:16:28 +0545

    fixing errors

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-const loginID = process.env.LOGIN
+const loginID = process.env.LOGIN;
 // mongoose.connect("mongodb://localhost:27017/blogDB");
-mongoose.connect(`mongodb+srv://admin-sandip:${loginID}@cluster0.bfgn0.mongodb.net/blogDB`);
+mongoose.connect('mongodb+srv://admin-sandip:'+loginID+'@cluster0.bfgn0.mongodb.net/blogDB');
 
 const postSchema = {
   title: String,
